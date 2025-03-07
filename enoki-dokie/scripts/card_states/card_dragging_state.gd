398930a03912ddcea46ceb877a8ab914592a0f95
cyschneck <22159116+cyschneck@users.noreply.github.com>
2025-03-07 00:42:49 -0700

    shrink slot bar to single box

--- enoki-dokie/scripts/card_states/card_dragging_state.gd
+++ enoki-dokie/scripts/card_states/card_dragging_state.gd
@@ -8,31 +8,34 @@ func enter() -> void:
 	# remove from hand while dragging
 	# reparent to "Card Manager" on dragging
 	var ui_layer := get_tree().get_nodes_in_group("ui_layer")
-	card.reparent(ui_layer[0])
+	card.reparent(ui_layer[0]) # parent to the CardManager during dragging
 
 	card.scale = Vector2(0.8, 0.8)
 	card.color.color = Color.NAVY_BLUE
 
-	# set min time to stay in dragging state
+	# set min time to stay in dragging state to be valid
 	min_drag_time_elapsed = false
 	var threshold_timer := get_tree().create_timer(DRAG_MIN_THRESHOLD, false)
 	threshold_timer.timeout.connect(func(): min_drag_time_elapsed = true)
 
 func on_input(event: InputEvent) -> void:
 	var mouse_motion := event is InputEventMouseMotion # true when mouse is moving
-	var cancel = event.is_action_pressed("right_mouse") # cancel movement with right click
-	var confirm = event.is_action_released("left_mouse") or event.is_action_pressed("left_mouse") # confirm by releasing left mouse
+	# cancel movement with right click
+	var cancel = event.is_action_pressed("right_mouse") 
+	# confirm by releasing left mouse
+	var confirm = event.is_action_released("left_mouse") or event.is_action_pressed("left_mouse") 
 
 	if mouse_motion:
 		# update global position of card while dragging
 		card.global_position = card.get_global_mouse_position() - card.pivot_offset
 	if cancel:
 		# if dragging is canceled, return to base state
-		# on cancel, reparent to HandCards
 		var ui_layer := get_tree().get_nodes_in_group("ui_layer")
+		# on cancel, reparent to HandCards
 		card.reparent(ui_layer[1])
 		transition_requested.emit(self, CardState.State.BASE)
 	if confirm and min_drag_time_elapsed:
+		# on confirm, set to RELEASED state
 		get_viewport().set_input_as_handled()
 		transition_requested.emit(self, CardState.State.RELEASED)
 			
--- enoki-dokie/scripts/card_states/card_released_state.gd
+++ enoki-dokie/scripts/card_states/card_released_state.gd
@@ -1,27 +1,30 @@
 extends CardState
 
-var card_played: bool
 var max_size_of_card_slot := 2
+var valid_card_played: bool
 
 func enter() -> void:
-	card_played = false # card not yet played
+	valid_card_played = false # card not yet played
 
 	var ui_layer := get_tree().get_nodes_in_group("ui_layer")
 
-	if not card.targets.is_empty() and not ui_layer[2].get_child_count() < max_size_of_card_slot:
-		# contains a valid target
-		card_played = true # card played on a valid release spot
+	# contains a valid target
+	if not card.targets.is_empty() and ui_layer[2].get_child_count() < max_size_of_card_slot:
+		valid_card_played = true # card played on a valid release spot
 	else:
-		card_played = false
+		valid_card_played = false
 
 func on_input(_event: InputEvent) -> void:
 	var ui_layer := get_tree().get_nodes_in_group("ui_layer")
 
-	if not card_played:
-		# reparent to "CardSlots" on release
+	if valid_card_played:
+		# valid placement for card
 		card.color.color = Color.DARK_MAGENTA
+		# reparent to "CardSlots" on release
 		card.reparent(ui_layer[2])
 	else:
+		# invalid placement for card
 		# if card is not played or if CardSlot is full
+		# reparent to HAND
 		card.reparent(ui_layer[1])
 		transition_requested.emit(self, CardState.State.BASE)
